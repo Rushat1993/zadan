936ca491d7d8910adc09641279059956d5cb7d3c
Rishat <risatnurullin53@gmail.com>
2022-07-12 17:38:15 +0300

    tyuu

---
+++ ty3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Введите 1-ое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 3-е число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine(num1);
+}
+else if (num2 > num3)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine(num3);
+}
