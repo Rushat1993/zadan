1fecee7eb8e99c931fd96ca2f3413b2bcb6019ee
Rishat <risatnurullin53@gmail.com>
2022-07-12 17:40:31 +0300

    tyuiii

---
+++ ty4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите 1-ое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 )
+{
+    Console.WriteLine("Первое число больше второго ");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого " );
+}
