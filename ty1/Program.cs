df6029886105191c8d62d56e98e3a485fb56d1a5
Rishat <risatnurullin53@gmail.com>
2022-07-12 17:35:39 +0300

    ty1

---
+++ ty1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 %2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное" );
+}
